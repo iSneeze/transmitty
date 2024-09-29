ad46c34badbc5716e3b634b5b7f9e1ed0f77ee1a
iSneeze <yeetallthespam@gmail.com>
2024-09-29 02:54:43 +0200

    formatting

--- script.js
+++ script.js
@@ -1,80 +1,80 @@
 
-var decodeRef = { 
-    '░█':     'a',
-    '█░░░':   'b',
-    '█░█░':   'c',
-    '█░░':    'd',
-    '░':      'e',
-    '░░█░':   'f',
-    '██░':    'g',
-    '░░░░':   'h',
-    '░░':     'i',
-    '░███':   'j',
-    '█░█':    'k',
-    '░█░░':   'l',
-    '██':     'm',
-    '█░':     'n',
-    '███':    'o',
-    '░██░':   'p',
-    '██░█':   'q',
-    '░█░':    'r',
-    '░░░':    's',
-    '█':      't',
-    '░░█':    'u',
-    '░░░█':   'v',
-    '░██':    'w',
-    '█░░█':   'x',
-    '█░██':   'y',
-    '██░░':   'z',
-    '░████':  '1',
-    '░░███':  '2',
-    '░░░██':  '3',
-    '░░░░█':  '4',
-    '░░░░░':  '5',
-    '█░░░░':  '6',
-    '██░░░':  '7',
-    '███░░':  '8',
-    '████░':  '9',
-    '█████':  '0',  
+var decodeRef = {
+    '░█': 'a',
+    '█░░░': 'b',
+    '█░█░': 'c',
+    '█░░': 'd',
+    '░': 'e',
+    '░░█░': 'f',
+    '██░': 'g',
+    '░░░░': 'h',
+    '░░': 'i',
+    '░███': 'j',
+    '█░█': 'k',
+    '░█░░': 'l',
+    '██': 'm',
+    '█░': 'n',
+    '███': 'o',
+    '░██░': 'p',
+    '██░█': 'q',
+    '░█░': 'r',
+    '░░░': 's',
+    '█': 't',
+    '░░█': 'u',
+    '░░░█': 'v',
+    '░██': 'w',
+    '█░░█': 'x',
+    '█░██': 'y',
+    '██░░': 'z',
+    '░████': '1',
+    '░░███': '2',
+    '░░░██': '3',
+    '░░░░█': '4',
+    '░░░░░': '5',
+    '█░░░░': '6',
+    '██░░░': '7',
+    '███░░': '8',
+    '████░': '9',
+    '█████': '0',
 };
 
-var encodeRef = { 
-    'a' : '░█'   ,
-    'b' : '█░░░' ,
-    'c' : '█░█░' ,
-    'd' : '█░░'  ,
-    'e' : '░'    ,
-    'f' : '░░█░' ,
-    'g' : '██░'  ,
-    'h' : '░░░░' ,
-    'i' : '░░'   ,
-    'j' : '░███' ,
-    'k' : '█░█'  ,
-    'l' : '░█░░' ,
-    'm' : '██'   ,
-    'n' : '█░'   ,
-    'o' : '███'  ,
-    'p' : '░██░' ,
-    'q' : '██░█' ,
-    'r' : '░█░'  ,
-    's' : '░░░'  ,
-    't' : '█'    ,
-    'u' : '░░█'  ,
-    'v' : '░░░█' ,
-    'w' : '░██'  ,
-    'x' : '█░░█' ,
-    'y' : '█░██' ,
-    'z' : '██░░' ,
-    '1' : '░████',
-    '2' : '░░███',
-    '3' : '░░░██',
-    '4' : '░░░░█',
-    '5' : '░░░░░',
-    '6' : '█░░░░',
-    '7' : '██░░░',
-    '8' : '███░░',
-    '9' : '████░',
-    '0' : '█████',  
+var encodeRef = {
+    'a': '░█',
+    'b': '█░░░',
+    'c': '█░█░',
+    'd': '█░░',
+    'e': '░',
+    'f': '░░█░',
+    'g': '██░',
+    'h': '░░░░',
+    'i': '░░',
+    'j': '░███',
+    'k': '█░█',
+    'l': '░█░░',
+    'm': '██',
+    'n': '█░',
+    'o': '███',
+    'p': '░██░',
+    'q': '██░█',
+    'r': '░█░',
+    's': '░░░',
+    't': '█',
+    'u': '░░█',
+    'v': '░░░█',
+    'w': '░██',
+    'x': '█░░█',
+    'y': '█░██',
+    'z': '██░░',
+    '1': '░████',
+    '2': '░░███',
+    '3': '░░░██',
+    '4': '░░░░█',
+    '5': '░░░░░',
+    '6': '█░░░░',
+    '7': '██░░░',
+    '8': '███░░',
+    '9': '████░',
+    '0': '█████',
 };
 
 function encode() {
@@ -84,13 +84,13 @@ function encode() {
     let cypher = humanField.value.toLowerCase()
         // seperate words
         .split(' ')
-        .map( word =>
+        .map(word =>
             // words -> letters
-            word.split('').map( letter => 
+            word.split('').map(letter =>
                 // tranlate each letter
-                /^[a-z0-9]*$/gi.test(letter) ? 
-                // put in the half blocks ▀
-                encodeRef[letter].split('').join('▀') : letter
+                /^[a-z0-9]*$/gi.test(letter) ?
+                    // put in the half blocks ▀
+                    encodeRef[letter].split('').join('▀') : letter
                 // seperate them with spaces
             ).join(' ')
         ).join(' ');
@@ -98,18 +98,18 @@ function encode() {
     mittyField.value = cypher;
 }
 
-function decode(){
+function decode() {
     let humanField = document.getElementById("humanReadable");
     let mittyField = document.getElementById("mittyCypher");
 
     let clean = mittyField.value.replaceAll('▀', '');
-    
+
     let decyphered = clean.split(' ')
-        .map( letter => 
-            !letter.includes('█') && !letter.includes('░') ? 
-            letter : decodeRef[letter]
+        .map(letter =>
+            !letter.includes('█') && !letter.includes('░') ?
+                letter : decodeRef[letter]
         ).join('');
-    
+
     humanField.value = decyphered
 
 }
